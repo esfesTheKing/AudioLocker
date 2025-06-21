056f10e9e7ffa7d05e0d4f69dca0c268fe0832d9
esfesTheKing <www.ilion26015gk@gmail.com>
2025-06-21 18:11:53 +0300

    Moved AudioSessionNotification to a different file
    Signed-off-by: esfesTheKing <www.ilion26015gk@gmail.com>

--- AudioLocker.Core/CoreAudioAPI/Wrappers/AudioSessionManager.cs
+++ AudioLocker.Core/CoreAudioAPI/Wrappers/AudioSessionManager.cs
@@ -1,18 +1,6 @@
 ﻿using AudioLocker.Core.CoreAudioAPI.Interfaces;
-using System.Runtime.InteropServices.Marshalling;
 
 namespace AudioLocker.Core.CoreAudioAPI.Wrappers;
-
-[GeneratedComClass]
-public partial class AudioSessionNotification(AudioSessionManager parent) : IAudioSessionNotification
-{
-    public void OnSessionCreated(IAudioSessionControl NewSession)
-    {
-        var session = new AudioSessionControl(NewSession);
-
-        parent.OnSessionCreated?.Invoke(parent, session);
-    }
-}
 
 public class AudioSessionManager : IDisposable
 {
---
+++ AudioLocker.Core/CoreAudioAPI/Wrappers/AudioSessionNotification.cs
@@ -0,0 +1,15 @@
+﻿using AudioLocker.Core.CoreAudioAPI.Interfaces;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace AudioLocker.Core.CoreAudioAPI.Wrappers;
+
+[GeneratedComClass]
+public partial class AudioSessionNotification(AudioSessionManager parent) : IAudioSessionNotification
+{
+    public void OnSessionCreated(IAudioSessionControl NewSession)
+    {
+        var session = new AudioSessionControl(NewSession);
+
+        parent.OnSessionCreated?.Invoke(parent, session);
+    }
+}
