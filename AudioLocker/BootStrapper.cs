79ac9136b9dbc3c2663d34fbf81eef39fbfad0cd
esfesTheKing <www.ilion26015gk@gmail.com>
2026-01-04 06:53:05 +0200

    Testing new icon + icon now contains 16,24,32,48 & 256 variants
    Signed-off-by: esfesTheKing <www.ilion26015gk@gmail.com>

--- AudioLocker/BootStrapper.cs
+++ AudioLocker/BootStrapper.cs
@@ -70,6 +70,8 @@ internal class BootStrapper
     private void InitializeTrayApp()
     {
         ApplicationConfiguration.Initialize();
+        Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
+
 #pragma warning disable WFO5001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
         Application.SetColorMode(SystemColorMode.System);
 #pragma warning restore WFO5001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
